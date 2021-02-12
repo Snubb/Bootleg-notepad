ef175064eaa4515b93b41d9beea43c274ecb2af8
Snubb <60389050+Snubb@users.noreply.github.com>
2021-02-12 10:55:39 +0100

    Initial commit

---
+++ src/ManagerGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class ManagerGUI {
+    private JTextArea textArea1;
+    private JPanel panel1;
+    private JButton saveButton;
+    private JButton openButton;
+    private JButton newButton;
+    private JButton clearButton;
+    private JButton button5;
+    private JButton button6;
+}
